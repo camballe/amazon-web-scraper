5731bd9a963dadfa07c38c9eae848d7b189d8c2e
Enoch Kambale <campnoch@gmail.com>
2023-09-18 10:03:57 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# amazon_web-scraper
+
+This is an API built with Bun and Express, that scrapes product data from the amazon website and parses it to JSON.
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run:
+
+```bash
+bun run index.ts
+```
+
+This project was created using `bun init` in bun v1.0.2. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
+
+
+Contact me for details on how to get API Key if you would like to use this API.
---
+++ index.ts
@@ -0,0 +1,85 @@
+import express from "express";
+import request from "request-promise";
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+// const apiKey: string = "d357fb7e0bf59330edc5a4f7dc0f34b6";
+
+const generateScraperUrl = (apiKey: string) =>
+  `http://api.scraperapi.com?api_key=${apiKey}&autoparse=true`;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Let's scrape Amazon🚀!");
+});
+
+// GET Product Details
+app.get("/products/:productId", async (req, res) => {
+  const { productId } = req.params;
+  const { api_key } = req.query;
+
+  try {
+    const response = await request(
+      `${generateScraperUrl(
+        api_key as string
+      )}&url=https://www.amazon.com/dp/${productId}`
+    );
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+// GET Product Reviews
+app.get("/products/:productId/reviews", async (req, res) => {
+  const { productId } = req.params;
+  const { api_key } = req.query;
+
+  try {
+    const response = await request(
+      `${generateScraperUrl(
+        api_key as string
+      )}&url=https://www.amazon.com/product-reviews/${productId}`
+    );
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+// GET Product Offers
+app.get("/products/:productId/offers", async (req, res) => {
+  const { productId } = req.params;
+  const { api_key } = req.query;
+
+  try {
+    const response = await request(
+      `${generateScraperUrl(
+        api_key as string
+      )}&url=https://www.amazon.com/gp/offer-listing/${productId}`
+    );
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+// GET Search Results
+app.get("/search/:searchQuery", async (req, res) => {
+  const { searchQuery } = req.params;
+  const { api_key } = req.query;
+
+  try {
+    const response = await request(
+      `${generateScraperUrl(
+        api_key as string
+      )}&url=https://www.amazon.com/s?k=${searchQuery}`
+    );
+    res.json(JSON.parse(response));
+  } catch (error) {
+    res.json(error);
+  }
+});
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
